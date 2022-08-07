d75c63d2917b55436313b9579b6ba809a7fdfdf0
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2022-08-07 11:14:10 +0200

    connectar till servern

--- server/index.js
+++ server/index.js
@@ -3,3 +3,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://Admin:Chiken82@cluster0.kqxj4o9.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+.then(()=> app.listen(PORT, () => console.log('Server running on Port: '+PORT)))
+.catch((error) => console.log(error));
+
